caaea34edbaa44e11366a8ec255ded9a4915ae4b
Lucas Rocha <ld.rocha@hotmail.com>
2020-01-28 17:03:43 -0800

    add shadow to cell

--- PetMe/AllPetsController.swift
+++ PetMe/AllPetsController.swift
@@ -11,7 +11,7 @@ import UIKit
 class AllPetsController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     private let cell_id = "pet_cell"
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -44,16 +44,22 @@ class AllPetsController: UICollectionViewController, UICollectionViewDelegateFlo
         //top, left, bottom, right
         return UIEdgeInsets(top: 10, left: 0, bottom: 0, right: 0)
     }
-
-
+    
+    
 }
 
 class PetCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = UIColor.white
-        self.layer.shadowColor = UIColor.black.cgColor
-        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        layer.cornerRadius = 5
+        layer.borderWidth = 0
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 1)
+        layer.shadowRadius = 5
+        layer.shadowOpacity = 0.2
+        
+        layer.masksToBounds = false //<-
         
         setupViews()
     }
@@ -86,12 +92,13 @@ class PetCell: UICollectionViewCell {
         addSubview(nameLabel)
         addSubview(dogImageView)
         
+        
         dogImageView.image = UIImage(named: "drika")
         
         dogImageView.translatesAutoresizingMaskIntoConstraints = false
         
         
-//        addContraintsWithFormat(format: "H:|-12-[v0(68)]", views: dogImageView)
+        //        addContraintsWithFormat(format: "H:|-12-[v0(68)]", views: dogImageView)
         addContraintsWithFormat(format: "V:[v0(68)]", views: dogImageView)
         
         //Center ImageView:
@@ -105,37 +112,37 @@ class PetCell: UICollectionViewCell {
 
 extension PetCell {
     func addContraintsWithFormat(format: String, views: UIView...) {
-
-           var viewsDictionary = [String:UIView]()
-
-           for (index, view) in views.enumerated() {
-               let key = "v\(index)"
-               viewsDictionary[key] = view
-               view.translatesAutoresizingMaskIntoConstraints = false
-           }
-
-           // Better to write code below just once ;)
-           addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: .init(), metrics: nil, views: viewsDictionary))
-       }
+        
+        var viewsDictionary = [String:UIView]()
+        
+        for (index, view) in views.enumerated() {
+            let key = "v\(index)"
+            viewsDictionary[key] = view
+            view.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        // Better to write code below just once ;)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: .init(), metrics: nil, views: viewsDictionary))
+    }
 }
 
 //MARK: - UIColor Extension
 
 extension UIColor {
-   convenience init(red: Int, green: Int, blue: Int) {
-       assert(red >= 0 && red <= 255, "Invalid red component")
-       assert(green >= 0 && green <= 255, "Invalid green component")
-       assert(blue >= 0 && blue <= 255, "Invalid blue component")
-
-       self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
-   }
-
-   convenience init(rgb: Int) {
-       self.init(
-           red: (rgb >> 16) & 0xFF,
-           green: (rgb >> 8) & 0xFF,
-           blue: rgb & 0xFF
-       )
-   }
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
 }
 
